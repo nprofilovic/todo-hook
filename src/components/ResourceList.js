0cc20e325cc10bd219f3b2a8e47eaf87a559e083
nprofilovic <nprofilovic@gmail.com>
2019-06-15 13:53:46 +0200

    Update files and make Resource List page

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
-class ResourceList extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.resource}
-            </div>
-        )
+
+const ResourceList = ({ resource }) => {
+    const [resources, setResource] = useState([]);
+
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+        setResource( response.data );
     }
+
+    useEffect(() => {
+        fetchResource(resource);
+    }, [resource])
+
+    return (
+        <div>
+            {resources.length}
+        </div>
+    )
+    
 }
 
 export default ResourceList
